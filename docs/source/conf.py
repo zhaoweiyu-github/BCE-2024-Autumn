aa30ba8a8011cbce9177e61cf9b8a7fd888ed8e6
zhaoweiyu-github <46152828+zhaoweiyu-github@users.noreply.github.com>
2024-09-03 10:47:41 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+import sphinx_rtd_theme
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BCE-2024-Autumn'
+copyright = '2024, ZhangLab'
+author = 'Zhaowei Yu'
 
 release = '0.1'
 version = '0.1.0'
@@ -17,6 +18,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "nbsphinx",
+    "nbsphinx_link",
+    "recommonmark",
+    "sphinx.ext.viewcode"
 ]
 
 intersphinx_mapping = {
@@ -30,6 +35,8 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+nbsphinx_execute = "never"
